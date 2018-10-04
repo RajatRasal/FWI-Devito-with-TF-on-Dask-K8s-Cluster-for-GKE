722b077beb0caf040ade63628d06c928ff7938ad
Rajat Rasal <yugiohrajat1@gmail.com>
2018-10-04 03:04:12 +0100

    Able to deploy on GKE without errors

--- config/jupyter-config.py
+++
@@ -1,8 +0,0 @@
-c.NotebookApp.password = '' 
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.token = ''
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8888
-c.NotebookApp.allow_root = True    # docker attach volume as root
-# c.NotebookApp.contents_manager_class = 'jgscm.GoogleStorageContentManager'
-# c.GoogleStorageContentManager.default_path = '<enter your gcs path without leading /, eg bucket_name/folder_name>'
