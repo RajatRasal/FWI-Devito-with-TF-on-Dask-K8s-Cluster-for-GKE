4436c16994320f907945f082fba3ef27a821e590
Rajat Rasal <yugiohrajat1@gmail.com>
2018-10-12 00:11:29 +0100

    Added jupyter-config file to be able to run notebooks in buckets

---
+++ config/jupyter-config.py
@@ -0,0 +1,8 @@
+c.NotebookApp.password = '' 
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.token = ''
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+c.NotebookApp.allow_root = True    # docker attach volume as root
+c.NotebookApp.contents_manager_class = 'jgscm.GoogleStorageContentManager'
+c.GoogleStorageContentManager.default_path = 'fwi-data'
